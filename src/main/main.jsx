a87564373471eb4d8a39700ce86568e29580195a
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-05-09 11:58:08 -0400

    fixed token submit

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -34,7 +34,8 @@ class Main extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.reqTok = this.reqTok.bind(this);
+    this.makeRequest = this.makeRequest.bind(this);
+    // this.requestToken = this.requestToken.bind(this);
     this.addOne = this.addOne.bind(this);
   }
 
@@ -43,22 +44,16 @@ class Main extends Component {
     this.setState({ search: e.target.value });
   }
 
-  async reqTok(){
-    try {
-      const result = await getNewToken();
-      debugger
-      return result;
-    }
-    catch(err){
-      console.log(err);
-    }
+  handleSubmit(e){
+    e.preventDefault();
+    getNewToken() // get new token first
+      .then(token => {
+        this.makeRequest(token);
+      });
   }
 
-  handleSubmit(e){
-    // e.persist()
+  makeRequest(token){
     const { search } = this.state;
-    let token = this.reqTok();
-    debugger
     const request = new Request(`https://api.spotify.com/v1/search?q=${search}&type=Artist`, {
       method: 'GET',
       headers: new Headers({
@@ -67,10 +62,8 @@ class Main extends Component {
         'Authorization': 'Bearer ' + token,
       }),
     });
-    debugger
     fetch(request)
       .then( res => {
-        debugger
         if (res.status >= 400) {
           const message = `Status: ${res.status}, Error: ${res.statusText}`;
           this.setState({ errors: message });
@@ -78,15 +71,10 @@ class Main extends Component {
         return res.json();
       })
       .then(data => {
-        console.log(data);
-        debugger
         const { name, images, id, genres, href } = data.artists.items[0];
         const artist = { name, id, images, genres, href };
         this.setState({ result: this.state.result.concat(artist) });
       });
-      e.preventDefault();
-      e.persist();
-
   }
 
   showErrors(){
@@ -115,12 +103,11 @@ class Main extends Component {
           />
         {this.showErrors()}
         <div>
-
+          <Graph nodes={this.state.result} links={this.state.links} />
         </div>
       </div>
     );
   }
 }
-// <Graph nodes={this.state.result} links={this.state.links} />
 
 export default Main;
